510b9b315935f346b85f4aa0a17d6d18afeebba2
addy07s <addy07.sharma@gmail.com>
2025-01-13 18:09:38 +0530

    Implement video upload functionality with error handling and enhance video retrieval filtering

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,7 +8,29 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const uploadVideo=asyncHandler(async (req,res) => {
     const{title, description}=req.body
-    //TODO
+
+    
+    try {
+
+        if(!req.file){
+            throw new ApiError(400,"no video file uploaded found")
+    }
+
+        const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+            resource_type: 'video',
+          });
+    
+          const video = await Video.create({
+            title,
+            description,
+            videoUrl: uploadResult.secure_url,
+            cloudinaryId: uploadResult.public_id,
+          });
+    
+          new ApiResponse(200,video,"video published successfully")
+    } catch (error) { throw new ApiError(500,"something went wrong while uploading video")
+        
+    }
     
 })
 
@@ -35,5 +57,15 @@ const getVideoById=asyncHandler(async (req,res) => {
 const getAllVideos= asyncHandler(async (req,res) => {
     
     const {page=1, limit=10, query, sortBy='createdAt', sortType='desc', userId }=req.query
+    const pageNum=parseInt(page)
+    const limitNum=parseInt(limit)
+
+    const filter = {
+        ...(query && { title: { $regex: query, $options: 'i' } }), // Case-insensitive search on title
+        ...(userId && { userId }), // Filter by userId if provided
+      };
 
 })
+
+
+export { uploadVideo, getVideoById, getAllVideos}
